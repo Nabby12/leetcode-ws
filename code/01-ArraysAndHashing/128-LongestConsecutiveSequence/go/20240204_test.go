2d1b3ed51dc250d56fa15f2c6ef0679d583e0bb9
Nabby12 <Nabby12@users.noreply.github.com>
2024-02-04 11:17:41 +0900

    add/longest-increasing-subsequence3

---
+++ code/01-ArraysAndHashing/128-LongestConsecutiveSequence/go/20240204_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_longestConsecutiveSequence3(t *testing.T) {
+	t.Parallel()
+
+	type input struct {
+		nums []int
+	}
+
+	test := []struct {
+		name  string
+		input input
+		want  int
+	}{
+		{
+			name: "example1",
+			input: input{
+				nums: []int{100, 4, 200, 1, 3, 2},
+			},
+			want: 4,
+		},
+		{
+			name: "example2",
+			input: input{
+				nums: []int{0, 3, 7, 2, 5, 8, 4, 6, 0, 1},
+			},
+			want: 9,
+		},
+	}
+
+	for _, tt := range test {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			got := longestConsecutiveSequence3(tt.input.nums)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Fatalf("\ngot : %v\nwant: %v", got, tt.want)
+			}
+		})
+	}
+}

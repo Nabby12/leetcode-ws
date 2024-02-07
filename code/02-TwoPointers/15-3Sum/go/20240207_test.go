efea68aed9b490b405339a2c57d1a95846477d05
Nabby12 <Nabby12@users.noreply.github.com>
2024-02-07 09:47:50 +0900

    add/threesum-3

---
+++ code/02-TwoPointers/15-3Sum/go/20240207_test.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_threeSum3(t *testing.T) {
+	t.Parallel()
+
+	type input struct {
+		nums []int
+	}
+
+	test := []struct {
+		name  string
+		input input
+		want  [][]int
+	}{
+		{
+			name: "example1",
+			input: input{
+				nums: []int{-1, 0, 1, 2, -1, -4},
+			},
+			want: [][]int{
+				{-1, -1, 2},
+				{-1, 0, 1},
+			},
+		},
+		{
+			name: "example2",
+			input: input{
+				nums: []int{0, 1, 1},
+			},
+			want: [][]int{},
+		},
+		{
+			name: "example3",
+			input: input{
+				nums: []int{0, 0, 0},
+			},
+			want: [][]int{
+				{0, 0, 0},
+			},
+		},
+	}
+
+	for _, tt := range test {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			got := threeSum3(tt.input.nums)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Fatalf("\ngot : %v\nwant: %v", got, tt.want)
+			}
+		})
+	}
+}

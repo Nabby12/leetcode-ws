e23c31629c05af178ad636371860291124b9020f
Nabby12 <Nabby12@users.noreply.github.com>
2024-02-10 17:03:15 +0900

    add/valid-parentheses

---
+++ code/03-Stack/20-ValidParentheses/go/20240210_test.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_validParentheses2(t *testing.T) {
+	t.Parallel()
+
+	type input struct {
+		s string
+	}
+
+	test := []struct {
+		name  string
+		input input
+		want  bool
+	}{
+		{
+			name: "example1",
+			input: input{
+				s: "()",
+			},
+			want: true,
+		},
+		{
+			name: "example2",
+			input: input{
+				s: "()[]{}",
+			},
+			want: true,
+		},
+		{
+			name: "example3",
+			input: input{
+				s: "(]",
+			},
+			want: false,
+		},
+		{
+			name: "example3",
+			input: input{
+				s: "){",
+			},
+			want: false,
+		},
+	}
+
+	for _, tt := range test {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			got := validParentheses2(tt.input.s)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Fatalf("\ngot : %v\nwant: %v", got, tt.want)
+			}
+		})
+	}
+}

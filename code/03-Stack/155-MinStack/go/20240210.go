843befebfea5bce12f002cd36dbe206ff0c8fa86
Nabby12 <Nabby12@users.noreply.github.com>
2024-02-10 18:05:57 +0900

    add/20240210

---
+++ code/03-Stack/155-MinStack/go/20240210.go
@@ -0,0 +1,30 @@
+package main
+
+// https://leetcode.com/problems/min-stack/
+type MinStack struct {
+	data []int
+}
+
+func Constructor() MinStack {
+	return MinStack{}
+}
+
+func (this *MinStack) Push(val int) {
+	this.data = append(this.data, val)
+}
+
+func (this *MinStack) Pop() {
+	this.data = this.data[0 : len(this.data)-1]
+}
+
+func (this *MinStack) Top() int {
+	return this.data[len(this.data)-1]
+}
+
+func (this *MinStack) GetMin() int {
+	minimum := this.data[0]
+	for _, val := range this.data {
+		minimum = min(val, minimum)
+	}
+	return minimum
+}

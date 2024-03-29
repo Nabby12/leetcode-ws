470cf903822114dd0a2caf6a964f17e2ebc31ead
Nabby12 <Nabby12@users.noreply.github.com>
2024-03-29 09:41:47 +0900

    add/evaluate-reverse-polish-notation

---
+++ code/03-Stack/150-EvaluateReversePolishNotation/go/20240329_test.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_evaluateReversePolishNotation3(t *testing.T) {
+	t.Parallel()
+
+	type input struct {
+		tokens []string
+	}
+
+	test := []struct {
+		name  string
+		input input
+		want  int
+	}{
+		{
+			name: "example1",
+			input: input{
+				tokens: []string{"2", "1", "+", "3", "*"},
+			},
+			want: 9,
+		},
+		{
+			name: "example2",
+			input: input{
+				tokens: []string{"4", "13", "5", "/", "+"},
+			},
+			want: 6,
+		},
+		{
+			name: "example3",
+			input: input{
+				tokens: []string{"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"},
+			},
+			want: 22,
+		},
+		{
+			name: "example4",
+			input: input{
+				tokens: []string{"18"},
+			},
+			want: 18,
+		}, {
+			name: "example5",
+			input: input{
+				tokens: []string{"3", "11", "+", "5", "-"},
+			},
+			want: 9,
+		},
+	}
+
+	for _, tt := range test {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			got := evaluateReversePolishNotation3(tt.input.tokens)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Fatalf("\ngot : %v\nwant: %v", got, tt.want)
+			}
+		})
+	}
+}

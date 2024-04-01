0c59eeb49cb146f5ce1f42ee89a0734191204c81
Nabby12 <Nabby12@users.noreply.github.com>
2024-04-01 09:49:39 +0900

    add/best-time-to-buy-and-sell-stock

---
+++ code/05-SlidingWindow/121-BestTimeToBuyAndSellStock/go/20240401.go
@@ -0,0 +1,18 @@
+package main
+
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+func bestTimeToBuyAndSellStock2(prices []int) int {
+	maxProfit := 0
+	l, r := 0, 1
+	for r < len(prices) {
+		buy, sell := prices[l], prices[r]
+		if buy < sell {
+			profit := sell - buy
+			maxProfit = max(maxProfit, profit)
+		} else {
+			l = r
+		}
+		r++
+	}
+	return maxProfit
+}

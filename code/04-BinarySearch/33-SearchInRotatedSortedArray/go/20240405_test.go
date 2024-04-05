4b3184e20b9cf36fb23ee5e82a6969ad7c5acab3
Nabby12 <Nabby12@users.noreply.github.com>
2024-04-05 09:30:33 +0900

    add/search-in-rotated-sorted-array

---
+++ code/04-BinarySearch/33-SearchInRotatedSortedArray/go/20240405_test.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_searchInRotatedSortedArray4(t *testing.T) {
+	t.Parallel()
+
+	type input struct {
+		nums   []int
+		target int
+	}
+
+	test := []struct {
+		name  string
+		input input
+		want  int
+	}{
+		{
+			name: "example1",
+			input: input{
+				nums:   []int{4, 5, 6, 7, 0, 1, 2},
+				target: 0,
+			},
+			want: 4,
+		},
+		{
+			name: "example2",
+			input: input{
+				nums:   []int{4, 5, 6, 7, 0, 1, 2},
+				target: 3,
+			},
+			want: -1,
+		},
+		{
+			name: "example3",
+			input: input{
+				nums:   []int{1},
+				target: 0,
+			},
+			want: -1,
+		},
+		{
+			name: "example4",
+			input: input{
+				nums:   []int{5, 1, 3},
+				target: 3,
+			},
+			want: 2,
+		},
+	}
+
+	for _, tt := range test {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			got := searchInRotatedSortedArray4(tt.input.nums, tt.input.target)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Fatalf("\ngot : %v\nwant: %v", got, tt.want)
+			}
+		})
+	}
+}

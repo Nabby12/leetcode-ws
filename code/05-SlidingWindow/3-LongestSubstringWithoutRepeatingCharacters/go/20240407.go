ef2543278d5d627ede52dcdb378d91f5cc473ceb
Nabby12 <Nabby12@users.noreply.github.com>
2024-04-07 14:26:03 +0900

    add/longest-substring-without-repeating-characters

---
+++ code/05-SlidingWindow/3-LongestSubstringWithoutRepeatingCharacters/go/20240407.go
@@ -0,0 +1,30 @@
+package main
+
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+
+func longestSubstringWithoutRepeatingCharacters(s string) int {
+	strs := []rune(s)
+	buf := []rune{}
+	ans := 0
+	l, r := 0, 0
+	for r < len(strs) {
+		right := strs[r]
+		for len(buf) > 0 && contains(buf, right) {
+			buf = buf[1:]
+			l++
+		}
+		buf = append(buf, right)
+		ans = max(ans, len(buf))
+		r++
+	}
+	return ans
+}
+
+func contains(runes []rune, target rune) bool {
+	for _, s := range runes {
+		if s == target {
+			return true
+		}
+	}
+	return false
+}

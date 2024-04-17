7e4c34915e5598001914713e28ee40611dbe6a89
Nabby12 <Nabby12@users.noreply.github.com>
2024-04-17 11:47:21 +0900

    add/longest-substring-without-repeating-characters

---
+++ code/05-SlidingWindow/3-LongestSubstringWithoutRepeatingCharacters/go/20240417_test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_longestSubstringWithoutRepeatingCharacters4(t *testing.T) {
+	t.Parallel()
+
+	type input struct {
+		s string
+	}
+
+	test := []struct {
+		name  string
+		input input
+		want  int
+	}{
+		{
+			name: "example1",
+			input: input{
+				s: "abcabcbb",
+			},
+			want: 3,
+		},
+		{
+			name: "example2",
+			input: input{
+				s: "bbbbb",
+			},
+			want: 1,
+		},
+		{
+			name: "example3",
+			input: input{
+				s: "pwwkew",
+			},
+			want: 3,
+		},
+	}
+
+	for _, tt := range test {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			got := longestSubstringWithoutRepeatingCharacters4(tt.input.s)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Fatalf("\ngot : %v\nwant: %v", got, tt.want)
+			}
+		})
+	}
+}

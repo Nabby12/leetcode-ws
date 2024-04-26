8481993d2118205ef8c094c4ca88ab7ecc685e80
Nabby12 <Nabby12@users.noreply.github.com>
2024-04-26 22:35:28 +0900

    add/container-width-most-water

---
+++ code/02-TwoPointers/11-ContainerWithMostWater/go/20240426_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_containerWithMostWater2(t *testing.T) {
+	t.Parallel()
+
+	type input struct {
+		height []int
+	}
+
+	test := []struct {
+		name  string
+		input input
+		want  int
+	}{
+		{
+			name: "example1",
+			input: input{
+				height: []int{1, 8, 6, 2, 5, 4, 8, 3, 7},
+			},
+			want: 49,
+		},
+		{
+			name: "example2",
+			input: input{
+				height: []int{1, 1},
+			},
+			want: 1,
+		},
+	}
+
+	for _, tt := range test {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			got := containerWithMostWater2(tt.input.height)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Fatalf("\ngot : %v\nwant: %v", got, tt.want)
+			}
+		})
+	}
+}

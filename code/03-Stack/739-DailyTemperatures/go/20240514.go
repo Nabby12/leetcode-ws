57e8578bb8be7ba9aa51a41150a1dc1666894955
Nabby12 <Nabby12@users.noreply.github.com>
2024-05-14 10:37:11 +0900

    add/daily-temperatures

---
+++ code/03-Stack/739-DailyTemperatures/go/20240514.go
@@ -0,0 +1,16 @@
+package main
+
+// https://leetcode.com/problems/daily-temperatures/description/
+func dailyTemperatures6(temperatures []int) []int {
+	ans := make([]int, len(temperatures))
+	stack := []int{}
+	for i, t := range temperatures {
+		for len(stack) > 0 && temperatures[stack[len(stack)-1]] < t {
+			lastIndex := stack[len(stack)-1]
+			ans[lastIndex] = i - lastIndex
+			stack = stack[0 : len(stack)-1]
+		}
+		stack = append(stack, i)
+	}
+	return ans
+}

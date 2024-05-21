7442c70bec45c633d183f57ecd9b71573fc01133
Nabby12 <Nabby12@users.noreply.github.com>
2024-05-21 14:49:54 +0900

    add/generate-parentheses

---
+++ code/03-Stack/22-GenerateParentheses/go/20240521.go
@@ -0,0 +1,24 @@
+package main
+
+// https://leetcode.com/problems/generate-parentheses/description/
+func generateParentheses10(n int) []string {
+	ans := []string{}
+	stack := make([]rune, n*2)
+	generate10(&ans, stack, n, 0, 0)
+	return ans
+}
+
+func generate10(ans *[]string, stack []rune, n int, open int, close int) {
+	if open+close == n*2 {
+		*ans = append(*ans, string(stack))
+		return
+	}
+	if open < n {
+		stack[open+close] = '('
+		generate10(ans, stack, n, open+1, close)
+	}
+	if close < open {
+		stack[open+close] = ')'
+		generate10(ans, stack, n, open, close+1)
+	}
+}

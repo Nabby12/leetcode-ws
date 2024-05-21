74be8543a9483e81f0d098543dd9fabf79519b07
Nabby12 <Nabby12@users.noreply.github.com>
2024-05-21 14:49:48 +0900

    fix/func-name

--- code/03-Stack/22-GenerateParentheses/go/20240512.go
+++ code/03-Stack/22-GenerateParentheses/go/20240512.go
@@ -4,20 +4,20 @@ package main
 func generateParentheses9(n int) []string {
 	ans := []string{}
 	stack := make([]rune, n*2)
-	generate10(&ans, stack, n, 0, 0)
+	generate9(&ans, stack, n, 0, 0)
 	return ans
 }
 
-func generate10(ans *[]string, stack []rune, n int, open int, close int) {
+func generate9(ans *[]string, stack []rune, n int, open int, close int) {
 	if open+close == n*2 {
 		*ans = append(*ans, string(stack))
 	}
 	if open < n {
 		stack[open+close] = '('
-		generate10(ans, stack, n, open+1, close)
+		generate9(ans, stack, n, open+1, close)
 	}
 	if close < open {
 		stack[open+close] = ')'
-		generate10(ans, stack, n, open, close+1)
+		generate9(ans, stack, n, open, close+1)
 	}
 }
